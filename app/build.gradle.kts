54f94a4d994bd6b391ac611e66fb7cbf4ae92a4a
Gamze Coşkun <gamzecoskkun@gmail.com>
2024-08-27 04:16:26 +0300

    Add Navigation Component dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,6 +26,8 @@ android {
             )
         }
     }
+
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -45,4 +47,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui)
 }
