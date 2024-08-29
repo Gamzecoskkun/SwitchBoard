0a2b9519aaf0c34553f1bf03f73b82b6019f4e60
Gamze Coşkun <gamzecoskkun@gmail.com>
2024-08-29 22:40:50 +0300

    Add StepsFragment with view binding

---
+++ app/src/main/java/com/gamzecoskun/switchboard/ui/steps/StepsFragment.kt
@@ -0,0 +1,15 @@
+package com.gamzecoskun.switchboard.ui.steps
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.gamzecoskun.switchboard.common.viewBinding
+import com.gamzecoskun.switchboard.databinding.FragmentStepsBinding
+
+class StepsFragment : Fragment() {
+    private val binding by viewBinding (FragmentStepsBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
