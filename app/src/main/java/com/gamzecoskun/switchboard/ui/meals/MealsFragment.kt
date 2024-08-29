146b9c9334e75f6b08af1dc59805f9137c81bce0
Gamze Coşkun <gamzecoskkun@gmail.com>
2024-08-29 22:38:29 +0300

    Initialize view binding in MealsFragment

---
+++ app/src/main/java/com/gamzecoskun/switchboard/ui/meals/MealsFragment.kt
@@ -0,0 +1,17 @@
+package com.gamzecoskun.switchboard.ui.meals
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.gamzecoskun.switchboard.common.viewBinding
+import com.gamzecoskun.switchboard.databinding.FragmentMealsBinding
+
+class MealsFragment : Fragment() {
+
+    private val binding by viewBinding(FragmentMealsBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
