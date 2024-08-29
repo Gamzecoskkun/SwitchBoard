c01ba971cc68c2b3fff521d89fd28f5c827018e2
Gamze Coşkun <gamzecoskkun@gmail.com>
2024-08-29 22:37:54 +0300

    Controlled bottom navigation visibility based on health control state.

--- app/src/main/java/com/gamzecoskun/switchboard/MainActivity.kt
+++ app/src/main/java/com/gamzecoskun/switchboard/MainActivity.kt
@@ -1,20 +1,93 @@
 package com.gamzecoskun.switchboard
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
+import android.view.Menu
+import android.view.View
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.gamzecoskun.switchboard.common.viewBinding
+import com.gamzecoskun.switchboard.databinding.ActivityMainBinding
+import com.gamzecoskun.switchboard.ui.healthcontrol.HealthControlViewModel
+import com.gamzecoskun.switchboard.ui.healthcontrol.NavigationItem
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by viewBinding(ActivityMainBinding::inflate)
+
+    private lateinit var navController: NavController
+
+    private lateinit var bottomNav: BottomNavigationView
+
+    private val viewModel: HealthControlViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        binding.bottomNav.setupWithNavController(navController)
+
+        bottomNav = binding.bottomNav
+
+        viewModel.addedItemsOrder.observe(this, Observer { updatedOrder ->
+            updateBottomNavigationView(updatedOrder)
+            Log.e("addedItems", updatedOrder.toString())
+        })
+
+        viewModel.healthControlChecked.observe(this, Observer { isChecked ->
+            bottomNavVisibility(isChecked)
+        })
+    }
+
+    private fun updateBottomNavigationView(order: List<NavigationItem>) {
+        val menu = bottomNav.menu
+        menu.clear()
+
+        val healthControlItem = NavigationItem.HEALTHCONTROL
+        menu.add(Menu.NONE, healthControlItem.fragmentId, Menu.NONE, "Health Control")
+            .setIcon(healthControlItem.iconRes)
+
+        var addedItemCount = 1
+        var toastShown = false
+
+        order.forEach { item ->
+            if (addedItemCount < 5) {
+                menu.add(Menu.NONE, item.fragmentId, addedItemCount, item.name)
+                    .setIcon(item.iconRes)
+                addedItemCount++
+            } else if (!toastShown) {
+                Toast.makeText(
+                    this,
+                    getString(R.string.limit_five),
+                    Toast.LENGTH_SHORT
+                ).show()
+                toastShown = true
+            }
+        }
+    }
+
+    private fun bottomNavVisibility(healtControlChecked: Boolean) {
+        if (healtControlChecked) {
+            binding.bottomNav.visibility = View.GONE
+        } else {
+            binding.bottomNav.visibility = View.VISIBLE
         }
     }
 }
+
+
+
+
+
+
+
+
