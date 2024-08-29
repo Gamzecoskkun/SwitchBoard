1ee6e5fa43be5f721948a7a16362492e91c62215
Gamze Coşkun <gamzecoskkun@gmail.com>
2024-08-29 22:40:22 +0300

    Implement SleepFragment with view binding

---
+++ app/src/main/java/com/gamzecoskun/switchboard/ui/sleep/SleepFragment.kt
@@ -0,0 +1,15 @@
+package com.gamzecoskun.switchboard.ui.sleep
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.gamzecoskun.switchboard.common.viewBinding
+import com.gamzecoskun.switchboard.databinding.FragmentSleepBinding
+
+class SleepFragment : Fragment() {
+    private val binding by viewBinding(FragmentSleepBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
