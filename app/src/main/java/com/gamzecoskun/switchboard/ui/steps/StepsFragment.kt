58d9a7a0cad4aee978772bcd4f11fb723a2ecb92
Gamze Coşkun <gamzecoskkun@gmail.com>
2024-08-30 18:55:53 +0300

    Add layout for steps fragment

--- app/src/main/java/com/gamzecoskun/switchboard/ui/steps/StepsFragment.kt
+++ app/src/main/java/com/gamzecoskun/switchboard/ui/steps/StepsFragment.kt
@@ -3,10 +3,11 @@ package com.gamzecoskun.switchboard.ui.steps
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.gamzecoskun.switchboard.R
 import com.gamzecoskun.switchboard.common.viewBinding
 import com.gamzecoskun.switchboard.databinding.FragmentStepsBinding
 
-class StepsFragment : Fragment() {
+class StepsFragment : Fragment(R.layout.fragment_steps) {
     private val binding by viewBinding (FragmentStepsBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
