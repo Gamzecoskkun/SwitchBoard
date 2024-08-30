d06cdbad9e1a7d6d5e6a4e91c120e19889c7e933
Gamze Coşkun <gamzecoskkun@gmail.com>
2024-08-30 18:54:53 +0300

    Add layout for exercises fragment

--- app/src/main/java/com/gamzecoskun/switchboard/ui/exercise/ExerciseFragment.kt
+++ app/src/main/java/com/gamzecoskun/switchboard/ui/exercise/ExerciseFragment.kt
@@ -4,10 +4,11 @@ package com.gamzecoskun.switchboard.ui.exercise
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.gamzecoskun.switchboard.R
 import com.gamzecoskun.switchboard.common.viewBinding
 import com.gamzecoskun.switchboard.databinding.FragmentExerciseBinding
 
-class ExerciseFragment : Fragment() {
+class ExerciseFragment : Fragment(R.layout.fragment_exercise) {
 
     private val binding by viewBinding (FragmentExerciseBinding::bind)
 
