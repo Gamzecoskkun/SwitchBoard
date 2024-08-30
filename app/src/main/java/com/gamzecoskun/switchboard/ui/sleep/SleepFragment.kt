b719567222ae3e940965f0b9423bcb929ffc34f1
Gamze Coşkun <gamzecoskkun@gmail.com>
2024-08-30 18:55:34 +0300

    Add layout for sleep fragment

--- app/src/main/java/com/gamzecoskun/switchboard/ui/sleep/SleepFragment.kt
+++ app/src/main/java/com/gamzecoskun/switchboard/ui/sleep/SleepFragment.kt
@@ -3,10 +3,11 @@ package com.gamzecoskun.switchboard.ui.sleep
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.gamzecoskun.switchboard.R
 import com.gamzecoskun.switchboard.common.viewBinding
 import com.gamzecoskun.switchboard.databinding.FragmentSleepBinding
 
-class SleepFragment : Fragment() {
+class SleepFragment : Fragment(R.layout.fragment_sleep) {
     private val binding by viewBinding(FragmentSleepBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
