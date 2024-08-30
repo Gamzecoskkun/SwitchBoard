4d9971398832b9c3701ba4314b5b043a0b76819a
Gamze Coşkun <gamzecoskkun@gmail.com>
2024-08-30 04:02:29 +0300

    Add clearAllSwitch method to reset all switch states to false in HealthControlViewModel

--- app/src/main/java/com/gamzecoskun/switchboard/ui/healthcontrol/HealthControlViewModel.kt
+++ app/src/main/java/com/gamzecoskun/switchboard/ui/healthcontrol/HealthControlViewModel.kt
@@ -17,38 +17,78 @@ class HealthControlViewModel : ViewModel() {
     private val _healthControlChecked = MutableLiveData<Boolean>(true)
     val healthControlChecked: LiveData<Boolean> get() = _healthControlChecked
 
+    private val _hydrationChecked = MutableLiveData<Boolean>(false)
+    val hydrationChecked: LiveData<Boolean> get() = _hydrationChecked
+
+    private val _exerciseChecked = MutableLiveData<Boolean>(false)
+    val exerciseChecked: LiveData<Boolean> get() = _exerciseChecked
+
+    private val _sleepChecked = MutableLiveData<Boolean>(false)
+    val sleepChecked: LiveData<Boolean> get() = _sleepChecked
+
+    private val _stepsChecked = MutableLiveData<Boolean>(false)
+    val stepsChecked: LiveData<Boolean> get() = _stepsChecked
+
+    private val _mealsChecked = MutableLiveData<Boolean>(false)
+    val mealsChecked: LiveData<Boolean> get() = _mealsChecked
+
     fun updateAddedItemsOrder(switchId: Int, isChecked: Boolean) {
         val currentOrder = _addedItemsOrder.value?.toMutableList() ?: mutableListOf()
 
-        if (switchId == R.id.healthControl) {
-            _healthControlChecked.value = isChecked
-            if (isChecked) {
-                resetAddedItemsOrder()
-                _addedItemsOrder.value = emptyList()
-            }
-        } else {
-            val item = when (switchId) {
-                R.id.meals -> NavigationItem.MEALS
-                R.id.exercise -> NavigationItem.EXERCISE
-                R.id.sleep -> NavigationItem.SLEEP
-                R.id.steps -> NavigationItem.STEPS
-                R.id.hydration -> NavigationItem.HYDRATION
-                else -> null
-            }
+        val previousOrder = currentOrder.toList()
 
-            if (item != null) {
+        when (switchId) {
+            R.id.healthControl -> {
+                _healthControlChecked.value = isChecked
                 if (isChecked) {
-                    currentOrder.add(item)
-                } else {
-                    currentOrder.remove(item)
+                    resetAddedItemsOrder()
+                }else{
+                    resetAddedItemsOrder()
+                    clearAllSwitch()
                 }
-                _addedItemsOrder.value = currentOrder
             }
+            R.id.hydration -> _hydrationChecked.value = isChecked
+            R.id.exercise -> _exerciseChecked.value = isChecked
+            R.id.sleep -> _sleepChecked.value = isChecked
+            R.id.steps -> _stepsChecked.value = isChecked
+            R.id.meals -> _mealsChecked.value = isChecked
+        }
+
+        val item = getNavigationItemForSwitch(switchId)
+        if (item != null) {
+            if (isChecked && !currentOrder.contains(item)) {
+                currentOrder.add(item)
+            } else if (!isChecked && currentOrder.contains(item)) {
+                currentOrder.remove(item)
+            }
+        }
+
+        if (currentOrder != previousOrder) {
+            _addedItemsOrder.value = currentOrder
+        }
+    }
+    private fun getNavigationItemForSwitch(switchId: Int): NavigationItem? {
+        return when (switchId) {
+            R.id.meals -> NavigationItem.MEALS
+            R.id.exercise -> NavigationItem.EXERCISE
+            R.id.sleep -> NavigationItem.SLEEP
+            R.id.steps -> NavigationItem.STEPS
+            R.id.hydration -> NavigationItem.HYDRATION
+            else -> null
         }
     }
 
     private fun resetAddedItemsOrder() {
         _addedItemsOrder.value = emptyList()
     }
+
+    private fun clearAllSwitch(){
+       _hydrationChecked.value=false
+        _stepsChecked.value=false
+        _sleepChecked.value=false
+        _mealsChecked.value=false
+        _exerciseChecked.value=false
+    }
 }
+
 
