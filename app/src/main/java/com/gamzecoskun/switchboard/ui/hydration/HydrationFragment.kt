0c27d98569975506b57e6527b9ed0678a504b1c0
Gamze Coşkun <gamzecoskkun@gmail.com>
2024-08-30 18:55:13 +0300

    Add layout for hydration fragment

--- app/src/main/java/com/gamzecoskun/switchboard/ui/hydration/HydrationFragment.kt
+++ app/src/main/java/com/gamzecoskun/switchboard/ui/hydration/HydrationFragment.kt
@@ -3,10 +3,11 @@ package com.gamzecoskun.switchboard.ui.hydration
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.gamzecoskun.switchboard.R
 import com.gamzecoskun.switchboard.common.viewBinding
 import com.gamzecoskun.switchboard.databinding.FragmentHydrationBinding
 
-class HydrationFragment : Fragment() {
+class HydrationFragment : Fragment(R.layout.fragment_hydration) {
 
     private val binding by viewBinding(FragmentHydrationBinding::bind)
 
