53c2694c736c541e2685655e53caef4f16a76b23
Gamze Coşkun <gamzecoskkun@gmail.com>
2024-08-30 16:31:17 +0300

    Update MealsFragment to use layout resource R.layout.fragment_meals

--- app/src/main/java/com/gamzecoskun/switchboard/ui/meals/MealsFragment.kt
+++ app/src/main/java/com/gamzecoskun/switchboard/ui/meals/MealsFragment.kt
@@ -3,10 +3,11 @@ package com.gamzecoskun.switchboard.ui.meals
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.gamzecoskun.switchboard.R
 import com.gamzecoskun.switchboard.common.viewBinding
 import com.gamzecoskun.switchboard.databinding.FragmentMealsBinding
 
-class MealsFragment : Fragment() {
+class MealsFragment : Fragment(R.layout.fragment_meals) {
 
     private val binding by viewBinding(FragmentMealsBinding::bind)
 
